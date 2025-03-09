96431dc1415b99a97507c94f18ccc52071f712e8
bayelaye313 <bayelaye0510@gmail.com>
2025-03-09 01:19:16 +0000

    prisma install and  user model and save register in db

--- actions/RegisterServer.ts
+++ actions/RegisterServer.ts
@@ -1,16 +1,33 @@
 "use server"
 
+import { db } from '@/lib/db'
 import { RegisterSchema } from '@/schemas'
 import { z } from 'zod'
+import bcrypt from 'bcrypt'
 
 const RegisterServer = async (values: z.infer<typeof RegisterSchema>) => {
     const ValidateFields = RegisterSchema.safeParse(values)
      if (!ValidateFields.success) {
         return {error: "invalid fields"}
     }
+    const {name, email, password} = ValidateFields.data
+
+    const isUserExist = await db.user.findUnique({
+        where: {email}
+    })
+    if (isUserExist) {
+        return {error: "User Already Exist"}
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    await db.user.create ({data:{
+        name,
+        email,
+        password : hashedPassword
+    }})
 
   return (
-    {success: "login success"}
+    {success: "user created"}
   )
 }
 
